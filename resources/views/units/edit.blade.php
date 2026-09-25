9b34a5d53d0da212582ceaad0b39b4a08e33e26f
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 22:05:42 +0430

    style: add final product page icon

--- resources/views/units/edit.blade.php
+++ resources/views/units/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-app.page
+    <x-app.page icon="cube"
         :title="__('units.edit_title')"
         :subtitle="__('units.subtitle')"
         :breadcrumbs="[
