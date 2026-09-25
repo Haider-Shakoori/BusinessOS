3efd3f29c5418081f154725debb53369821f69b8
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 22:06:13 +0430

    style: add final product page icon

--- resources/views/quotations/show.blade.php
+++ resources/views/quotations/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-app.page
+    <x-app.page icon="document-text"
         :title="$quotation->quotation_number"
         :subtitle="__('quotations.document')"
         :breadcrumbs="[
