898d11da30f775de2f9f8791c8b623120be2500c
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 22:06:49 +0430

    style: add final product page icon

--- resources/views/expenses/show.blade.php
+++ resources/views/expenses/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-app.page
+    <x-app.page icon="arrow-trending-down"
         :title="$expense->expense_number"
         :subtitle="__('expenses.information')"
         :breadcrumbs="[
