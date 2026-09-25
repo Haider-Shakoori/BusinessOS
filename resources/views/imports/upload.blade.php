67f3d1b9219c5e44bd6ebb920657278fdd49cda1
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 22:06:59 +0430

    style: add final product page icon

--- resources/views/imports/upload.blade.php
+++ resources/views/imports/upload.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-app.page
+    <x-app.page icon="arrow-up-tray"
         :title="$title"
         :subtitle="$subtitle"
         :breadcrumbs="[
