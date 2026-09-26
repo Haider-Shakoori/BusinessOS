049ef782b20c2088b6bbc46ecad555d3c84389ab
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-26 08:25:46 +0430

    feat: add product shell metadata

---
+++ config/product.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'version' => env('BUSINESSOS_VERSION', 'v1.0.0'),
+    'batch_label' => env('BUSINESSOS_BATCH_LABEL', 'Batch 26'),
+    'plan_label' => env('BUSINESSOS_PLAN_LABEL'),
+];
