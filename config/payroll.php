097efde1877f2632bf32777ec4a672adf6c755ec
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-26 13:18:48 +0430

    feat: use Friday-off default payroll workweek

--- config/payroll.php
+++ config/payroll.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default_working_days' => [1, 2, 3, 4, 5],
+    'default_working_days' => [1, 2, 3, 4, 6, 7],
     'overtime_multiplier' => 1.5,
     'default_standard_daily_minutes' => 480,
 ];
