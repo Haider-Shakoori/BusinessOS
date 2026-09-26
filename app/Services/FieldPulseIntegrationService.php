ce8ad172adcf044d65a68787a7de09765b7192fd
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-26 21:52:57 +0430

    Avoid conflicting unary style in UUID check

--- app/Services/FieldPulseIntegrationService.php
+++ app/Services/FieldPulseIntegrationService.php
@@ -683,7 +683,7 @@ class FieldPulseIntegrationService
     {
         $uuid = trim((string) ($data['fieldpulse_uuid'] ?? ''));
 
-        if (! Str::isUuid($uuid)) {
+        if (Str::isUuid($uuid) === false) {
             throw new InvalidArgumentException(
                 'FieldPulse entity UUID is required.',
             );
