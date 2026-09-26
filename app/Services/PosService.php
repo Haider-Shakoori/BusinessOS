f73be1ed5e5049b33a7a11953dc6bf099b5ec3d8
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-26 18:59:38 +0430

    chore: remove duplicate inventory return import

--- app/Services/PosService.php
+++ app/Services/PosService.php
@@ -7,7 +7,6 @@ use App\Enums\ProductType;
 use App\Models\Account;
 use App\Models\Customer;
 use App\Models\InventoryReturn;
-use App\Models\InventoryReturn;
 use App\Models\JournalEntry;
 use App\Models\PosRegister;
 use App\Models\PosSale;
